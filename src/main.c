4e8340b6c314abbf020e73d1427d9e0768180481
Bardauil-Monetti <gbardauil548@gmail.com>
2025-11-25 13:18:08 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,24 +12,24 @@ int main(void){
 
     AFIO->EXTICR[0] &= ~(0xF << (sen1 * 4));     
     AFIO->EXTICR[0] |=  (0x0 << (sen1 * 4));    
-    AFIO->EXTICR[1] &= ~(0xF << (sen2 * 4));     
-    AFIO->EXTICR[1] |=  (0x0 << (sen2 * 4));     
+    AFIO->EXTICR[0] &= ~(0xF << (sen2 * 4));     
+    AFIO->EXTICR[0] |=  (0x0 << (sen2 * 4));     
 
-    AFIO->EXTICR[2] &= ~(0xF << (normal * 4));    
-    AFIO->EXTICR[2] |=  (0x0 << (normal * 4));    
-    AFIO->EXTICR[3] &= ~(0xF << (restringido * 4)); 
-    AFIO->EXTICR[3] |=  (0x0 << (restringido * 4)); 
-    AFIO->EXTICR[4] &= ~(0xF << ((libre % 4) * 4));//del EXTICR[4] en adelante, se declaran diferente a los anteriores 
-    AFIO->EXTICR[4] |=  (0X2 << ((libre % 4) * 4));// 0X2 = PC (es el puerto en el que esta seteado el pin)
+    AFIO->EXTICR[0] &= ~(0xF << (normal * 4));    
+    AFIO->EXTICR[0] |=  (0x0 << (normal * 4));    
+    AFIO->EXTICR[1] &= ~(0xF << (restringido * 4)); 
+    AFIO->EXTICR[1] |=  (0x0 << (restringido * 4)); 
+    AFIO->EXTICR[1] &= ~(0xF << ((libre % 4) * 4));//del EXTICR[4] en adelante, se declaran diferente a los anteriores 
+    AFIO->EXTICR[1] |=  (0X2 << ((libre % 4) * 4));// 0X2 = PC (es el puerto en el que esta seteado el pin)
   
     estInit();
-    for(int i = 1; i < 5; i++){
+    for(int i = 1; i < 6; i++){
         EXTI->IMR  |= (1 << i);
         EXTI->RTSR |= (1 << i);   
         EXTI->FTSR &= ~(1 << i);
     }
     
-    NVIC_EnableIRQ(EXTI0_IRQn);
+    NVIC_EnableIRQ(EXTI9_5_IRQn);
     NVIC_EnableIRQ(EXTI1_IRQn);
     NVIC_EnableIRQ(EXTI2_IRQn);
     NVIC_EnableIRQ(EXTI3_IRQn);
