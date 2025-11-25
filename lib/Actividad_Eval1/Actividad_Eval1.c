519781fbdc6764d3a1839c73d04f879b0db9b81a
Bardauil-Monetti <gbardauil548@gmail.com>
2025-11-25 12:58:20 -0300

    Update GPIOB BSRR setting in Actividad_Eval1.c

--- lib/Actividad_Eval1/Actividad_Eval1.c
+++ lib/Actividad_Eval1/Actividad_Eval1.c
@@ -65,6 +65,7 @@ void teclado(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4){
                 }
                 max = uni + dec + cen;
             }
+        GPIOB->BSRR |= (1 << (filas[i] + 16));    
         }
     }
 }
